6a1ce01cd5a4ed690d89b6096e8e975b10a58007
wanhao <wanhao1208@gmail.com>
2013-07-02 18:05:23 +0800

    solution of 3.3

---
+++ src/chapter3/Q3_3.java
@@ -0,0 +1,45 @@
+package chapter3;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Q3_3 {
+
+  public static void main(String[] args) {
+    
+    
+  }
+  
+  static class SetOfStack<E>{
+    private ArrayList<Stack<E>> stacks=new ArrayList<Stack<E>>();
+    
+    private int maxSize=0;
+    public SetOfStack(int maxElementPerStack){
+      this.maxSize=maxElementPerStack;
+    }
+    
+    public void push(E item){
+      Stack<E> stack=null;
+      if(stacks.isEmpty() || (stack=stacks.get(stacks.size()-1)).size()>=maxSize){
+        stack=new Stack<E>();
+        stacks.add(stack);
+      }
+      stack.push(item);
+    }
+    
+    public E pop(){
+      if(stacks.isEmpty())return null;
+      Stack<E> stack=stacks.get(stacks.size()-1);
+      
+      E item=stack.pop();
+      if(stack.empty()){
+        stacks.remove(stacks.size()-1);
+      }
+      
+      return item;
+    }
+    
+    
+  }
+
+}
